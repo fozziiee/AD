0c346401cd33c7fff6f7601354b8f9ec60053943
fozziiee <kaydefoster@gmail.com>
2025-06-29 20:35:28 +1000

    Add bootstrap script for generating AD environment

---
+++ code/bootstrap_ad.ps1
@@ -0,0 +1,13 @@
+param(
+    [int]$Users = 5,
+    [int]$Groups = 2,
+    [int]$Admins = 1
+)
+
+Write-Host "📦 Generating AD environment JSON..."
+
+.\random_domain.ps1 -OutputJSONFile .\env.json -UserCount $Users -GroupCount $Groups -LocalAdminCount $Admins
+
+Write-Host "🏗 Creating AD environment from JSON schema..."
+
+.\gen_ad.ps1 -JSONFile .\env.json
