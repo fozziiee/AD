4e3b7ca234452751b94c18e84bb253ee1c9bf4fe
fozziiee <kaydefoster@gmail.com>
2025-06-30 12:03:52 +1000

    Remove emoji from output messages in bootstrap_ad.ps1

--- code/bootstrap_ad.ps1
+++ code/bootstrap_ad.ps1
@@ -4,10 +4,10 @@ param(
     [int]$Admins = 1
 )
 
-Write-Host "📦 Generating AD environment JSON..."
+Write-Host "Generating AD environment JSON..."
 
 .\random_domain.ps1 -OutputJSONFile .\env.json -UserCount $Users -GroupCount $Groups -LocalAdminCount $Admins
 
-Write-Host "🏗 Creating AD environment from JSON schema..."
+Write-Host "Creating AD environment from JSON schema..."
 
 .\gen_ad.ps1 -JSONFile .\env.json
