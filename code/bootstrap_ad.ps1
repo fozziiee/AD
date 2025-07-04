ef6bb313b9032c423be83ad94da64acc32b69de3
fozziiee <kaydefoster@gmail.com>
2025-07-04 22:35:01 +1000

    Add comments for AD DS role installation and server promotion in bootstrap_ad.ps1; create .gitignore to exclude my-notes.md

--- code/bootstrap_ad.ps1
+++ code/bootstrap_ad.ps1
@@ -4,6 +4,18 @@ param(
     [int]$Admins = 1
 )
 
+# # Install AD DS Role
+# Install-WindowsFeature -Name AD_Domain-Services -IncludeManagementTools
+
+# # Promote server to  Domain Controller
+# Install-ADDSForest `
+#     -DomainName "cyberlab.local" `
+#     -SafeModeAdministratorPassword (ConvertTo-SecureString 'P@ssw0rd123' -AsPlainText -Force) `
+#     -Force $true
+
+# # Everything after this will NOT run until after reboot unless you handle that with scheduled task or second stage script
+
+
 Write-Host "Generating AD environment JSON..."
 
 .\random_domain.ps1 -OutputJSONFile .\env.json -UserCount $Users -GroupCount $Groups -LocalAdminCount $Admins
