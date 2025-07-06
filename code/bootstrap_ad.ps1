992ca8953c05687cfd970a6a58b8261551a0b8fe
fozziiee <kaydefoster@gmail.com>
2025-07-06 23:38:36 +1000

    adjusted sleep timer

--- code/bootstrap_ad.ps1
+++ code/bootstrap_ad.ps1
@@ -15,7 +15,7 @@ param(
 
 # # Everything after this will NOT run until after reboot unless you handle that with scheduled task or second stage script
 
-Start-Sleep -Seconds 30
+Start-Sleep -Seconds 120
 
 $logPath = "C:\cyberlab\AD\AD.log"
 Start-Transcript -Path $logPath -Append
