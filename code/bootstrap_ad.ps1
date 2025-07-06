3e667775be7b18f171ec1d2f378d2b941bb4937f
fozziiee <kaydefoster@gmail.com>
2025-07-06 18:54:47 +1000

    updated log path

--- code/bootstrap_ad.ps1
+++ code/bootstrap_ad.ps1
@@ -15,7 +15,7 @@ param(
 
 # # Everything after this will NOT run until after reboot unless you handle that with scheduled task or second stage script
 
-$logPath = "C:\cyberlab\AD"
+$logPath = "C:\cyberlab\AD\AD.log"
 Start-Transcript -Path $logPath -Append
 
 Write-Host "Generating AD environment JSON..."
