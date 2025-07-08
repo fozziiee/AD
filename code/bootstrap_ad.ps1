ab5faeca57a405a883bd10175d1bc0ad203fb91b
fozziiee <kaydefoster@gmail.com>
2025-07-08 21:03:24 +1000

    Shifted sleep timer to be included in transcript

--- code/bootstrap_ad.ps1
+++ code/bootstrap_ad.ps1
@@ -15,10 +15,11 @@ param(
 
 # # Everything after this will NOT run until after reboot unless you handle that with scheduled task or second stage script
 
-Start-Sleep -Seconds 300
 
 $logPath = "C:\cyberlab\AD\AD.log"
 Start-Transcript -Path $logPath -Append
+
+Start-Sleep -Seconds 300
 
 Write-Host "Generating AD environment JSON..."
 
